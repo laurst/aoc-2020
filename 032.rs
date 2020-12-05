90a7bf8b50915fd635915aedf5a325a205483b9d
Laurent Sebellin <laurent.sebellin@stylight.com>
2020-12-05 17:37:07 +0100

    day 3 : forgot part 1

--- 032.rs
+++ 032.rs
@@ -15,11 +15,24 @@ fn main() {
     let input_lines: Vec<&str> = input_data.split_whitespace().collect();
 
     let width = input_lines[0].len();
-    let mut count = 0;
+
+    let inputs = [
+        (1, 1),
+        (3, 1),
+        (5, 1),
+        (7, 1),
+        (1, 2),
+    ];
+    let mut counts = [0; 5];
 
     for (index, line) in input_lines.iter().enumerate() {
-        let col = (index * 3) % width;
-        if line.chars().nth(col).unwrap() == '#' { count += 1 };
+        for (counter_index, (x_step, y_step)) in inputs.iter().enumerate() {
+            if index % y_step == 1 { continue };
+            let col = (index * x_step / y_step) % width;
+            if line.chars().nth(col).unwrap() == '#' { counts[counter_index] += 1 };
+        }
     }
-    println!("{}", count);
+    let result: u64 = counts.iter().fold(1, |a, b| a*b);
+    println!("{:?}", counts);
+    println!("{}", result);
 }
